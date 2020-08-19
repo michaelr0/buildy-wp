146cda75e6b1c30db7faf32c74f4c808d3a7eab1
Johannes <john@inertiainmotion.com.au>
2020-08-19 15:13:10 +1000

    Build & Version Bump

--- buildy-wp.php
+++ buildy-wp.php
@@ -5,7 +5,7 @@
  * Plugin Name: Buildy for WP
  * Plugin URI:
  * Description: Just like a chocolate milk, only apples!
- * Version:     2.2.4
+ * Version:     2.2.5
  * Author:
  * Author URI:
  * Text Domain: buildy-wp
