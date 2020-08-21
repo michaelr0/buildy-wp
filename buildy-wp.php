5b26043ab7024cd33c7f5c669fab2036f0c2b3e9
Johannes <john@inertiainmotion.com.au>
2020-08-22 00:24:39 +1000

    Style tweaks for baseline

--- buildy-wp.php
+++ buildy-wp.php
@@ -5,7 +5,7 @@
  * Plugin Name: Buildy for WP
  * Plugin URI:
  * Description: Just like a chocolate milk, only apples!
- * Version:     2.2.9
+ * Version:     2.2.10
  * Author:
  * Author URI:
  * Text Domain: buildy-wp
