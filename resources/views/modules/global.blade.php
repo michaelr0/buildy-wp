a301aed5af605014c645ac2ceac493af48181626
Michael Rook <michael@rook.net.au>
2020-08-27 13:51:07 +1000

    Allow adding classes to Global via wrapper div

--- resources/views/modules/global.blade.php
+++ resources/views/modules/global.blade.php
@@ -1,3 +1,21 @@
 @if(!empty($bladeData->content->id))
-    {!! $buildy->renderFrontend($bladeData->content->id) !!}
+    {{--
+      $bladeData->id
+      $bladeData->type
+      $bladeData->content
+      $bladeData->content->id
+      $bladeData->content->options
+      $bladeData->content->options->isEditable
+      $bladeData->content->options->admin_label
+      $bladeData->content->attributes
+      $bladeData->content->attributes->id
+      $bladeData->content->attributes->class
+      $bladeData->content->generatedAttributes
+      $bladeData->content->generatedAttributes->columns
+      $bladeData->content->generatedAttributes->spacing
+    --}}
+
+    <div>
+      {!! $buildy->renderFrontend($bladeData->content->id) !!}
+    </div>
 @endif
