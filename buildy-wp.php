e672d65d2b62a8cc999aac7b22ffb2e69286ab23
Johannes <john@inertiainmotion.com.au>
2020-08-28 12:47:56 +1000

    Make spacer utils !important

--- buildy-wp.php
+++ buildy-wp.php
@@ -5,7 +5,7 @@
  * Plugin Name: Buildy for WP
  * Plugin URI:
  * Description: Just like a chocolate milk, only apples!
- * Version:     2.2.18
+ * Version:     2.2.19
  * Author:
  * Author URI:
  * Text Domain: buildy-wp
