c4b6f1000e5aa081045c384d0a20352a9168053d
Johannes <john@inertiainmotion.com.au>
2020-09-15 17:12:34 +1000

    Remove accidental text output in global file

--- buildy-wp.php
+++ buildy-wp.php
@@ -5,7 +5,7 @@
  * Plugin Name: Buildy for WP
  * Plugin URI:
  * Description: Just like a chocolate milk, only apples!
- * Version:     2.2.24
+ * Version:     2.2.25
  * Author:
  * Author URI:
  * Text Domain: buildy-wp
