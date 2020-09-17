1cb6d21890787768e1e57a078a402cbf53b9965b
Johannes <john@inertiainmotion.com.au>
2020-09-17 15:29:04 +1000

    Fix misspelling

--- resources/views/modules/global.blade.php
+++ resources/views/modules/global.blade.php
@@ -8,7 +8,7 @@ $bgSize = (!empty($bladeData->inline->backgroundImage->backgroundSize)) ? $blade
 $bgPosition = (!empty($bladeData->inline->backgroundImage->backgroundPosition)) ? $bladeData->inline->backgroundImage->backgroundPosition : "";
 $bgRepeat = $bladeData->inline->backgroundImage->backgroundRepeat ?: null;
 $bgColor = (!empty($bladeData->inline->backgroundColor)) ? $bladeData->inline->backgroundColor : "";
-$bgImageSize = (!empty($bladeData->inline->backgroundImage->imageSize)) ? $bladeData->inline->backgroundImage->imageSize : "full";
+$bgImageSize = $bladeData->inline->backgroundImage->imageSize ? $bladeData->inline->backgroundImage->imageSize : "full";
 $bgImageURL = (!empty($bladeData->inline->backgroundImage->url)) ? $bladeData->inline->backgroundImage->url : null;
 $bgImageID = $bladeData->inline->backgroundImage->imageID ?? null;
 
@@ -48,27 +48,11 @@ if ($spacing) {
 }
 @endphp
 @if(!empty($bladeData->content->id))
-    {{--
-      $bladeData->id
-      $bladeData->type
-      $bladeData->content
-      $bladeData->content->id
-      $bladeData->content->options
-      $bladeData->content->options->isEditable
-      $bladeData->content->options->admin_label
-      $bladeData->content->attributes
-      $bladeData->content->attributes->id
-      $bladeData->content->attributes->class
-      $bladeData->content->generatedAttributes
-      $bladeData->content->generatedAttributes->columns
-      $bladeData->content->generatedAttributes->spacing
-    --}}
-
     <div
     style="
     @if($bgColor) {{ "background-color: $bgColor;" }} @endif
     @if($bgImage) {{ "background-image: url($bgImage);" }} @endif
-    @if($bgSize) {{ "backround-size: $bgSize;" }} @endif
+    @if($bgSize) {{ "background-size: $bgSize;" }} @endif
     @if($bgPosition) {{ "background-position: $bgPosition;" }} @endif
     @if($bgRepeat) {{ "background-repeat: $bgRepeat;" }} @endif"
     class="bmcb-global-wrapper {{ $moduleClasses ? $moduleClasses : '' }}">
