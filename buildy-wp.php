ec4eda345b493041da75507513bcd58faa082686
Johannes <john@handmadeweb.com.au>
2020-09-25 13:50:47 +1000

    Update mainFields defaults

--- buildy-wp.php
+++ buildy-wp.php
@@ -5,7 +5,7 @@
  * Plugin Name: Buildy for WP
  * Plugin URI:
  * Description: Just like a chocolate milk, only apples!
- * Version:     2.3.10
+ * Version:     2.3.11
  * Author:
  * Author URI:
  * Text Domain: buildy-wp
